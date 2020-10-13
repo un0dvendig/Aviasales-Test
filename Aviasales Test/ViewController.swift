e153b24d98794f3fb13a8c8a460c5ea69afa84c4
Eugene Ilyin <evgen.ilyin@gmail.com>
2020-10-13 18:08:54 +0300

    Initial Commit

---
+++ Aviasales Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aviasales Test
+//
+//  Created by Eugene Ilyin on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
